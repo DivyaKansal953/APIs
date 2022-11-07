737e63e654ee1339b3a3782a7df138fe6a74a651
DivyaKansal953 <115632534+DivyaKansal953@users.noreply.github.com>
2022-11-07 17:08:24 +0530

    Add files via upload

---
+++ GetRequest.java
@@ -0,0 +1,25 @@
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class GetRequest {
+
+	@Test
+	void getBooksDetails() {
+		//specify base uri
+		RestAssured.baseURI = "https://rahulshettyacademy.com/";
+		
+		//Request object
+		RequestSpecification httpRequest = RestAssured.given();
+	    
+		//Response object
+		Response response =httpRequest.request(Method.GET,"/Library/GetBook.php?AuthorName=shetty");
+	     
+		//print response in console window
+		String responseBody = response.getBody().asString();
+		System.out.println("Response body is:"+responseBody);
+	}
+}
